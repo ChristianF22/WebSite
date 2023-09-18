eb12954672ee4d92d71820d0db5ce82ad3acc051
CHRISTIAN FONSECA DA ROCHA PEREIRA <1-2022122060>
2023-09-18 09:55:40 -0300

    Nova pagina

---
+++ linkesButtonCar.js
@@ -0,0 +1,20 @@
+const LinkButton1 = document.getElementById("button1")
+const LinkButton2 = document.getElementById("button2")
+const LinkButton3 = document.getElementById("button3")
+const LinkButton4 = document.getElementById("button4")
+
+LinkButton1.addEventListener("click", function(){
+    window.location.href= "pagina2/pagina02.html"
+})
+
+LinkButton2.addEventListener("click", function(){
+    window.location.href= "pagina2/pagina02.html"
+})
+
+LinkButton3.addEventListener("click", function(){
+    window.location.href= "pagina2/pagina02.html"
+})
+
+LinkButton4.addEventListener("click", function(){
+    window.location.href= "pagina2/pagina02.html"
+})
